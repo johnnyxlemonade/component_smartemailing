781aad3c52b41e5cb8c30afafa4a219ced3d6839
Johnny X. Lemonade <johnnyxlemonade@users.noreply.github.com>
2025-12-03 10:40:19 +0100

    refactor SmartEmailingResponse

--- src/SmartEmailingResponse.php
+++ src/SmartEmailingResponse.php
@@ -5,18 +5,38 @@ namespace Lemonade\SmartEmailing;
 final class SmartEmailingResponse
 {
     public function __construct(
-        public readonly bool    $success,
-        public readonly mixed   $data = null,
-        public readonly ?string $message = null
+        private readonly bool    $success,
+        private readonly mixed   $data = null,
+        private readonly ?string $message = null
     ) {}
 
     public static function ok(mixed $data = null): self
     {
-        return new self(true, $data);
+        return new self(true, $data, null);
     }
 
     public static function error(string $message, mixed $data = null): self
     {
         return new self(false, $data, $message);
+    }
+
+    public function isSuccess(): bool
+    {
+        return $this->success;
+    }
+
+    public function hasError(): bool
+    {
+        return !$this->success;
+    }
+
+    public function getData(): mixed
+    {
+        return $this->data;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
     }
 }
